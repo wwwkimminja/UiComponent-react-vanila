5413c81b8148d76dd50f20bbe6167c8fbbfdcea2
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-03-23 20:20:29 +0900

    feat: routes

---
+++ src/app/routes.ts
@@ -0,0 +1,52 @@
+const routePaths = ['/','/test1','/test2','/test2/vanilla','/test2/react']as const ;
+type ROUTE_PATH = (typeof routePaths)[number];
+type BaseRoute = {
+  key:ROUTE_PATH
+  link:ROUTE_PATH
+  name:string
+}
+export type ParentRoute = BaseRoute & {
+  children:ROUTE_PATH[]
+}
+export type ChildRoute = BaseRoute & {
+  children:((props:unknown)=>JSX.Element)|null
+}
+export type ROUTE = ParentRoute |ChildRoute
+export const routes:Record<ROUTE_PATH,ROUTE>= 
+  {
+    '/':{
+      key: '/',
+      link: '/',
+      name:'ROOT',
+      children: ['/test1','/test2']
+
+    },
+    '/test1':{
+      key: '/test1',
+      link: '/test1',
+      name:'TEST1',
+      children: null
+    },
+    '/test2':{
+      key: '/test2',
+      link: '/test2',
+      name:'TEST2',
+      children: ['/test2/vanilla','/test2/react']
+    },
+    '/test2/vanilla':{
+      key: '/test2/vanilla',
+      link: '/test2/vanilla',
+      name:'Vanilla',
+      children: null
+    },
+    '/test2/react':{
+      key: '/test2/react',
+      link: '/test2/react',
+      name:'React',
+      children: null
+    },
+  }
+
+  export const isParentRoute = (route:ROUTE):route is ParentRoute => Array.isArray(route) 
+
+  export const gnbRootList = (routes['/']as ParentRoute).children.map(r=>routes[r])
